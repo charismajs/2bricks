15e700d43d43c2cc5e6df93a16fb235f5e4fccdb
kp <zestime@gmail.com>
2014-05-07 13:24:22 +0900

    Added Executor Controller (cherry picked from commit 5aec587)

--- server/controller/baseController.js
+++ server/controller/baseController.js
@@ -2,49 +2,52 @@
  * Created by kp on 21/04/2014.
  */
 
-module.exports = function(model) {
-    var getFn = function (id, next) {
-        model.findOne({_id: id}).exec(function (err, data) {
-            next(data);
-        });
-    };
+module.exports = function (model) {
+  var getFn = function (id, next) {
+    model.findOne({_id: id}).exec(function (err, data) {
+      next(data);
+    });
+  };
 
-    var result = {
-        create: function (req, res, next) {
-            var data = req.body;
+  var result = {
+    create: function (req, res, next) {
+      var data = req.body;
 
-            model.create(data, function (err, result) {
-                if (err) {
-                    res.send(err);
-                }
-
-                res.send(result);
-            })
-        },
-
-        getList : function (req, res, next) {
-            model.find(function (err, list) {
-                if (err)
-                    res.send(err);
-
-                res.json(list);
-            })
-        },
-
-        get : getFn,
-
-        getOverHttp : function (req, res, next) {
-            var id = req.params.name;
-            getFn(id, function(data) {
-                if (data == null) {
-                    res.status(404).json({status: 'Not found - ' + id});
-                }
-                res.json(data);
-            });
+      model.create(data, function (err, result) {
+        if (err) {
+          res.send(err);
         }
-    };
 
-    // TODO - Make put API for updating
+        if (next)
+          next(result);
+        else
+          res.send(result);
+      })
+    },
 
-    return result;
+    getList: function (req, res, next) {
+      model.find(function (err, list) {
+        if (err)
+          res.send(err);
+
+        res.json(list);
+      })
+    },
+
+    get: getFn,
+
+    getOverHttp: function (req, res, next) {
+      var id = req.params.name;
+      getFn(id, function (data) {
+        if (data == null) {
+          res.status(404).json({status: 'Not found - ' + id});
+        }
+        res.json(data);
+      });
+    }
+  };
+
+  // TODO - Make put API for updating
+
+  return result;
 };
