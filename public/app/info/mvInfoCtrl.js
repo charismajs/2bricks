b9ffab8343ad8ade13add2db13193dc948b1537e
charismajs <charismajs@gmail.com>
2014-05-07 10:02:47 +0900

    modify button in modal

--- public/app/info/mvInfoCtrl.js
+++ public/app/info/mvInfoCtrl.js
@@ -2,10 +2,17 @@ angular.module('app').controller('mvInfoCtrl', function ($scope, $modalInstance,
 
     $scope.execution = execution;
 
-    $scope.command = execution.status === 'start' ? 'stop' : 'start';
+    $scope.command = execution.status === 'begin' ? 'STOP' : 'START';
 
     $scope.run = function () {
+        // TODO : do start or stop according to the status of execution
+
+        // if execution's status is stop then start
         mvExecution.create({command: execution.command});
+
+        // TODO : Stop the execution
+        // if execution's status is start then stop
+
 
         $modalInstance.close();
     };
