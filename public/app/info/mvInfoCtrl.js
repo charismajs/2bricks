156943046eea63aca1af507335d5dceaf2ea4bd1
LuckyJS <charismajs@gmail.com>
2014-05-23 15:36:59 +0900

    modify modal ui  : add tab  : add file read function

--- public/app/info/mvInfoCtrl.js
+++ public/app/info/mvInfoCtrl.js
@@ -1,7 +1,11 @@
 angular.module('app').controller('mvInfoCtrl',
   function ($scope, $modalInstance, mvModelApi, execution) {
 
+    $scope.tabName = 'info';
     $scope.execution = execution;
+    $scope.content = '';
+    $scope.name = '';
+
     mvModelApi.Task.get({id:$scope.execution.taskId.toString()}, function(task) {
       $scope.task = task;
       mvModelApi.Job.get({id:$scope.task.jobId}, function(job) {
@@ -9,12 +13,17 @@ angular.module('app').controller('mvInfoCtrl',
       });
     });
 
+    $scope.showContent = function($fileContent){
+      $scope.content = $fileContent;
+    };
+
     $scope.start = function () {
       var data = mvModelApi.Execution.create(
         {
           command: execution.command,
           start: new Date()
         });
+
       $modalInstance.close(data);
     };
 
@@ -30,3 +39,25 @@ angular.module('app').controller('mvInfoCtrl',
       $modalInstance.dismiss('cancel');
     };
   });
+
+angular.module('app').directive('onReadFile', function ($parse) {
+  return {
+    restrict: 'A',
+    scope: false,
+    link: function (scope, element, attrs) {
+      var fn = $parse(attrs.onReadFile);
+
+      element.on('change', function (onChangeEvent) {
+        var reader = new FileReader();
+
+        reader.onload = function (onLoadEvent) {
+          scope.$apply(function () {
+            fn(scope, {$fileContent: onLoadEvent.target.result});
+          });
+        };
+
+        reader.readAsText((onChangeEvent.srcElement || onChangeEvent.target).files[0]);
+      });
+    }
+  };
+});
